36835035e478eec02b59446cab5ce24a877f56b2
LonewolfASIF <asifaliali0989191@gmail.com>
2023-01-13 23:21:13 +0530

    changes in submitbutton and elevated buildsubmit

--- lib/screens/login_setup/signup/otp/components/otp_form_screen.dart
+++ lib/screens/login_setup/signup/otp/components/otp_form_screen.dart
@@ -1,7 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:packmore/screens/login_setup/signup/otp/components/opt_screen_top_image.dart';
+import 'package:packmore/screens/login_setup/signup/otp/components/verify_phone_and_otp.dart';
+import 'package:packmore/screens/login_setup/welcome/welcome_screen.dart';
 import '../../../../../master_components/constants.dart';
 import '../../../../../master_components/size_config.dart';
+import '../../../../bottom_navigation_bar/body.dart';
+import '../../../../home_screen_details/components/body.dart';
+import '../../../../main_home_screen/components/categories.dart';
+import '../../../../main_home_screen/home_screen.dart';
+
+final currentUser = FirebaseAuth.instance.currentUser;
+final DocumentReference phoneNumberRef =
+    FirebaseFirestore.instance.collection('Users').doc(currentUser?.uid);
+
 
 class OtpFormScreen extends StatefulWidget {
   const OtpFormScreen({Key? key}) : super(key: key);
@@ -11,7 +26,25 @@ class OtpFormScreen extends StatefulWidget {
 }
 
 class _OtpFormScreenState extends State<OtpFormScreen> {
+  static String? _phoneNumber;
+  String verificationReceivedID = "";
+  String countryDial = "+91";
+
+  final _otpController = TextEditingController();
+  final currentUser = FirebaseAuth.instance.currentUser;
   final _formKey = GlobalKey<FormState>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  @override
+  void initState() {
+    super.initState();
+    _loadPhoneNumber();
+  }
+   Future<void> _loadPhoneNumber() async {
+     var collection = FirebaseFirestore.instance.collection('Users');
+    var docSnapshot = await collection.doc(currentUser?.uid).get();
+    Map<String, dynamic> data = docSnapshot.data()!;
+    _phoneNumber = data['PhoneNumber'];
+  }
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -24,11 +57,67 @@ class _OtpFormScreenState extends State<OtpFormScreen> {
           ],
         ));
   }
+// ElevatedButton buildSubmitButton() {
+//     return ElevatedButton(
+//         onPressed: () async {
+//             if (_formKey.currentState!.validate()) {
+//                 final User? currentUser = _auth.currentUser;
+//                 String enteredOTP = _otpController.text;
+//                 try {
+//                     AuthCredential credential = PhoneAuthProvider.credential(
+//                         verificationId: verificationReceivedID,
+//                         smsCode: enteredOTP,
+//                     );
+//                     await currentUser?.linkWithCredential(credential);
+//                     // OTP verification successful, navigate to home screen
+//                     Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                             builder: (context) => const HomeScreen(),
+//                         ),
+//                     );
+//                 } on FirebaseAuthException catch (error) {
+//                     Fluttertoast.showToast(
+//                         msg: error.message.toString(),
+//                         gravity: ToastGravity.TOP,
+//                     );
+//                 }
+//             }
+//         },
+//         child: Text(
+//             "Submit".toUpperCase(),
+//             style: const TextStyle(
+//                 fontWeight: FontWeight.normal, color: kPrimaryLightColor),
+//         ),
+//     );
+// }
 
   ElevatedButton buildSubmitButton() {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text(
+    return ElevatedButton( 
+      onPressed: () async {
+    final User? currentUser = _auth.currentUser;
+    String enteredOTP = _otpController.text;
+    try {
+      AuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verificationReceivedID,
+        smsCode: enteredOTP,
+      );
+      await currentUser?.linkWithCredential(credential);
+      // OTP verification successful, navigate to home screen
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    } on FirebaseAuthException catch (error) {
+        Fluttertoast.showToast(
+          msg: error.message.toString(),
+          gravity: ToastGravity.TOP,
+        );
+      }
+},
+       child: Text(
         "Submit".toUpperCase(),
         style: const TextStyle(
             fontWeight: FontWeight.normal, color: kPrimaryLightColor),
@@ -36,8 +125,21 @@ class _OtpFormScreenState extends State<OtpFormScreen> {
     );
   }
 
+  // void _onButtonPressed() {
+  //   setState(() {
+      
+  //   });
+  // }
+
   TextFormField buildOTPFormField(BuildContext context) {
     return TextFormField(
+       controller: _otpController,
+      validator: (value) {
+        if (value != verificationReceivedID) {
+          return 'Invalid OTP';
+        }
+        return null;
+      },
       cursorColor: kPrimaryColor,
       decoration: const InputDecoration(
           hintText: "Enter OTP",
@@ -63,7 +165,38 @@ class _OtpFormScreenState extends State<OtpFormScreen> {
       },
     );
   }
+  void verifyNumber() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      try {
+        await _auth.verifyPhoneNumber(
+            phoneNumber: countryDial + _phoneNumber.toString(),
+            verificationCompleted: (PhoneAuthCredential credential) async {
+              _auth.signInWithCredential(credential).then(
+                    (value) => Fluttertoast.showToast(msg: "Login Successful"),
+                  );
+            },
+            verificationFailed: (FirebaseAuthException exception) {
+              Fluttertoast.showToast(
+                msg: exception.message.toString(),
+                gravity: ToastGravity.TOP,
+              );
+            },
+            codeSent: (String verificationID, int? resendToken) {
+              verificationReceivedID = verificationID;
+              setState(() {});
+            },
+            codeAutoRetrievalTimeout: (String verificationID) {});
+      } on FirebaseAuthException catch (error) {
+        Fluttertoast.showToast(
+          msg: error.message.toString(),
+          gravity: ToastGravity.TOP,
+        );
+      }
+    }
+  }
 }
+
 
 
 // class OtpFormScreen extends StatefulWidget {
