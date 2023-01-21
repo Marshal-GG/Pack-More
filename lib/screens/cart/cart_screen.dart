6772649950b6b72b058a9b3f03a896e52bdb2aca
LonewolfASIF <asifaliali0989191@gmail.com>
2023-01-21 22:54:22 +0530

    created cart page

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:packmore/master_components/constants.dart';
+import 'package:packmore/master_components/size_config.dart';
 import 'package:packmore/models/Cart.dart';
+import 'components/body.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -9,6 +14,8 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context),
+      body: Body(),
+      bottomNavigationBar: CheckOutCard(),
     );
   }
 
@@ -19,7 +26,7 @@ class CartScreen extends StatelessWidget {
       title: Column(
         children: [
           const Text(
-            "Your Cart",
+            "My Cart ^_^",
             style: TextStyle(color: kPrimaryColor),
           ),
           Text(
@@ -30,6 +37,100 @@ class CartScreen extends StatelessWidget {
       ),
       backgroundColor: kPrimaryLightColor,
       centerTitle: true,
+    );
+  }
+}
+
+class CheckOutCard extends StatelessWidget {
+  const CheckOutCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: getProportionateScreenWidth(15),
+          horizontal: getProportionateScreenWidth(30),
+        ),
+        //height: 174,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, -15),
+              blurRadius: 20,
+              color: Colors.purple.shade100.withOpacity(0.15),
+            ),
+          ],
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    height: getProportionateScreenWidth(40),
+                    width: getProportionateScreenWidth(40),
+                    decoration: BoxDecoration(
+                      color: kPrimaryLightColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: SvgPicture.asset("assets/icons/receipt.svg"),
+                  ),
+                  Spacer(),
+                  Text("Add Voucher Code"),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    size: 12,
+                    color: kTextColor,
+                  )
+                ],
+              ),
+              SizedBox(
+                height: getProportionateScreenHeight(20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      text: "Total: \n",
+                      style: TextStyle(fontSize: 18, color: Colors.black54),
+                      children: [
+                        TextSpan(
+                          text: "\₹1000.00",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: kPrimaryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: getProportionateScreenWidth(190),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "CHECK  OUT",
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/cart/components/cart_item_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:packmore/models/Cart.dart';
+import '../../../master_components/constants.dart';
+import '../../../master_components/size_config.dart';
+
+class CartItemCard extends StatelessWidget {
+  const CartItemCard({
+    Key? key,
+    required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: getProportionateScreenWidth(88),
+          child: AspectRatio(
+            aspectRatio: 0.88,
+            child: Container(
+              decoration: BoxDecoration(
+                color: cart.product.color,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Image.asset(cart.product.image),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: getProportionateScreenWidth(20),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              cart.product.title,
+              style: TextStyle(fontSize: 16, color: Colors.black),
+              maxLines: 2,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text.rich(
+              TextSpan(
+                  text: "\₹${cart.product.price}",
+                  style: TextStyle(
+                    color: kPrimaryColor,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: " x${cart.numOfItems}",
+                      style: TextStyle(
+                        color: kTextColor,
+                      ),
+                    ),
+                  ]),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
